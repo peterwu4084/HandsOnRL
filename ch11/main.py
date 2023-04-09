c7a8ae708d3fefbbaf11f2996036d36b86f1e0fe
peterwu4084 <peterwu4084@outlook.com>
2023-04-09 12:59:44 +0800

    update ch11

--- ch11/main.py
+++ ch11/main.py
@@ -17,31 +17,31 @@ kl_constraint = 0.0005
 alpha = 0.5
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
-# env_name = 'CartPole-v0'
-# env = gym.make(env_name)
-# env.seed(0)
-# torch.manual_seed(0)
+env_name = 'CartPole-v0'
+env = gym.make(env_name)
+env.seed(0)
+torch.manual_seed(0)
 
-# agent = TRPO(hidden_dim, env.observation_space, env.action_space, lmbda,
-#              kl_constraint, alpha, critic_lr, gamma, device)
-# return_list = train_on_policy_agent(env, agent, num_episodes)
+agent = TRPO(hidden_dim, env.observation_space, env.action_space, lmbda,
+             kl_constraint, alpha, critic_lr, gamma, device)
+return_list = train_on_policy_agent(env, agent, num_episodes)
 
-# torch.save(agent.actor.state_dict(), 'actor_cartpolev0.pth')
-# torch.save(agent.critic.state_dict(), 'critic_cartpolev0.pth')
+torch.save(agent.actor.state_dict(), 'actor_cartpolev0.pth')
+torch.save(agent.critic.state_dict(), 'critic_cartpolev0.pth')
 
-# episodes_list = list(range(len(return_list)))
-# plt.plot(episodes_list, return_list)
-# plt.xlabel('Episodes')
-# plt.ylabel('Returns')
-# plt.title('TPRO on {}'.format(env_name))
-# plt.show()
+episodes_list = list(range(len(return_list)))
+plt.plot(episodes_list, return_list)
+plt.xlabel('Episodes')
+plt.ylabel('Returns')
+plt.title('TRPO on {}'.format(env_name))
+plt.show()
 
-# mv_return = moving_average(return_list, 9)
-# plt.plot(episodes_list, mv_return)
-# plt.xlabel('Episodes')
-# plt.ylabel('Returns')
-# plt.title('TPRO on {}'.format(env_name))
-# plt.show()
+mv_return = moving_average(return_list, 9)
+plt.plot(episodes_list, mv_return)
+plt.xlabel('Episodes')
+plt.ylabel('Returns')
+plt.title('TRPO on {}'.format(env_name))
+plt.show()
 
 
 num_episodes = 2000
@@ -66,12 +66,12 @@ episodes_list = list(range(len(return_list)))
 plt.plot(episodes_list, return_list)
 plt.xlabel('Episodes')
 plt.ylabel('Returns')
-plt.title('TPRO on {}'.format(env_name))
+plt.title('TRPO on {}'.format(env_name))
 plt.show()
 
 mv_return = moving_average(return_list, 9)
 plt.plot(episodes_list, mv_return)
 plt.xlabel('Episodes')
 plt.ylabel('Returns')
-plt.title('TPRO on {}'.format(env_name))
+plt.title('TRPO on {}'.format(env_name))
 plt.show()
